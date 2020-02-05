3ad6931e81076b9e704876c8bbca7372e58101b4
Soroshkin Anton <soroshkin_a_m@mail.ru>
2020-02-05 14:49:52 +0300

    Fix variable types to interface references

--- src/com/epam/online/test/CalculatorTest.java
+++ src/com/epam/online/test/CalculatorTest.java
@@ -3,16 +3,18 @@ package com.epam.online.test;
 import com.epam.online.exception.NotValidInputException;
 import com.epam.online.service.calculation.Calculation;
 import com.epam.online.service.calculation.ICalculation;
+import com.epam.online.service.parser.IStringParser;
 import com.epam.online.service.parser.StringParser;
+import com.epam.online.service.validator.IValidator;
 import com.epam.online.service.validator.MathExpressionsValidator;
 import com.epam.online.util.ConsoleHelper;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        ICalculation calculation = new Calculation();
-        LinkedList<String> expression = new LinkedList<>();
+        Queue<String> expression = new LinkedList<>();
         expression.add("1*5.00(4)");
         expression.add("(-3) + 5 * ((-8) - 1)^2"); //402
         expression.add("(-(5.6565))");
@@ -43,14 +45,17 @@ public class CalculatorTest {
         expression.add("(4,2 + 2^3) * 3 / 3 - 6.1");
         expression.add("1+5.5(3)");
 
+        IValidator mathExpressionsValidator = new MathExpressionsValidator();
+        IStringParser stringParser = new StringParser();
+        ICalculation calculation = new Calculation();
+
         expression.forEach((element) -> {
             try {
                 element = element.replaceAll("\\s", "");
-                MathExpressionsValidator mathExpressionsValidator = new MathExpressionsValidator();
                 mathExpressionsValidator.validate(element);
-                ConsoleHelper.printResult(calculation.calculate(new StringParser().parse(element)));
+                ConsoleHelper.printResult(calculation.calculate(stringParser.parse(element)));
             } catch (NotValidInputException | ArithmeticException e) {
-                System.err.printf("%s: %s\n", e.getClass().getSimpleName(), e.getMessage());
+                System.err.println(e.getMessage());
             }
         });
 
