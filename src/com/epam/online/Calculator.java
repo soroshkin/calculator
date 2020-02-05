37342fabe5ac8941213608c30cc1a68093e7f540
Soroshkin Anton <soroshkin_a_m@mail.ru>
2020-02-05 14:45:24 +0300

    Fix Calculator class

--- src/com/epam/online/Calculator.java
+++ src/com/epam/online/Calculator.java
@@ -2,6 +2,9 @@ package com.epam.online;
 
 import com.epam.online.exception.NotValidInputException;
 import com.epam.online.service.calculation.Calculation;
+import com.epam.online.service.calculation.ICalculation;
+import com.epam.online.service.parser.IStringParser;
+import com.epam.online.service.validator.IValidator;
 import com.epam.online.service.validator.MathExpressionsValidator;
 import com.epam.online.service.parser.StringParser;
 import com.epam.online.util.ConsoleHelper;
@@ -9,21 +12,20 @@ import com.epam.online.util.ConsoleHelper;
 
 public class Calculator {
     // Command used to exit the application
-    private static final String exitApplicationCommand = "quit";
+    private static final String EXIT_APPLICATION_COMMAND = "quit";
 
     public static void main(String[] args) {
-        while (true) {
-            ConsoleHelper.enterExpression(exitApplicationCommand);
-            String inputString;
-            if (!(inputString = ConsoleHelper.readInputString()).equalsIgnoreCase(exitApplicationCommand)) {
-                try {
-                    new MathExpressionsValidator().validate(inputString);
-                    ConsoleHelper.printResult(new Calculation().calculate(new StringParser().parse(inputString)));
-                } catch (NotValidInputException e) {
-                    System.err.printf("%s: %s\n", e.getClass().getSimpleName(), e.getMessage());
-                }
-            } else {
-                break;
+        IValidator mathExpressionsValidator = new MathExpressionsValidator();
+        IStringParser stringParser = new StringParser();
+        ICalculation calculation = new Calculation();
+        String inputString;
+
+        while (!(inputString = ConsoleHelper.enterAndReadExpression(EXIT_APPLICATION_COMMAND)).equalsIgnoreCase(EXIT_APPLICATION_COMMAND)) {
+            try {
+                mathExpressionsValidator.validate(inputString);
+                ConsoleHelper.printResult(calculation.calculate(stringParser.parse(inputString)));
+            } catch (NotValidInputException | ArithmeticException e) {
+                System.err.println(e.getMessage());
             }
         }
     }
