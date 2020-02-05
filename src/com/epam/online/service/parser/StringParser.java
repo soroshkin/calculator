6a3b7f01451d3d9305ec10599c24c4bfd6385997
Soroshkin Anton <soroshkin_a_m@mail.ru>
2020-02-05 14:47:46 +0300

    Split parse method in StringParser

--- src/com/epam/online/service/parser/StringParser.java
+++ src/com/epam/online/service/parser/StringParser.java
@@ -10,6 +10,13 @@ import static com.epam.online.enums.MathOperator.*;
 import static com.epam.online.enums.Messages.INCORRECT_BRACKETS;
 
 public class StringParser implements IStringParser {
+    Deque<String> expressionToCalculate;
+    Deque<String> mathOperationsStack;
+
+    public StringParser() {
+        expressionToCalculate = new LinkedList<>();
+        mathOperationsStack = new LinkedList<>();
+    }
 
     /**
      * Parses given String to expression in Reverse Polish notation (RPN) form
@@ -18,63 +25,123 @@ public class StringParser implements IStringParser {
      * @return Deque of strings, which represent expression in RPN form
      * @throws IncorrectBracketsException if brackets put incorrectly
      */
+    @Override
     public Deque<String> parse(String inputString) throws IncorrectBracketsException {
+        clearStacks();
+        StringBuilder expressionString = new StringBuilder(inputString);
 
-        Deque<String> expressionToCalculate = new LinkedList<>();
-        Deque<String> mathOperationsStack = new LinkedList<>();
-
-        while (inputString.length() > 0) {
-            Matcher matcher = DIGIT_PATTERN.getPattern().matcher(inputString);
-            if (matcher.find()) {
-                inputString = inputString.substring(matcher.end());
-                expressionToCalculate.push(matcher.group());
+        while (expressionString.length() > 0) {
+            if (isDigitOrUnaryMinusFound(expressionString)) {
                 continue;
             }
 
-            if (matcher.usePattern(UNARY_MINUS_PATTERN.getPattern()).find()) {
-                inputString = inputString.substring(matcher.end());
-                mathOperationsStack.push(OPEN_BRACKET);
-                expressionToCalculate.push(String.valueOf(0));
-                mathOperationsStack.push(MINUS.getOperator());
-                continue;
-            }
+            String currentSymbol = expressionString.substring(0, 1);
+            expressionString.deleteCharAt(0);
 
-            String currentSymbol = inputString.substring(0, 1);
-            inputString = inputString.substring(1);
-
-            if (currentSymbol.equals(OPEN_BRACKET)) {
-                mathOperationsStack.push(currentSymbol);
-                continue;
-            }
-
-            if (currentSymbol.equals(CLOSED_BRACKET)) {
-                String lastOperatorInStack;
-                try {
-                    while (!(lastOperatorInStack = mathOperationsStack.pop()).equals(OPEN_BRACKET)) {
-                        expressionToCalculate.push(lastOperatorInStack);
-                    }
-                } catch (NoSuchElementException e) {
-                    throw new IncorrectBracketsException(INCORRECT_BRACKETS.toString());
-                }
-                continue;
-            }
-
-            if (isMathOperation(currentSymbol)) {
-                String lastOperatorInStack;
-                while ((lastOperatorInStack = mathOperationsStack.peek()) != null
-                        && isMathOperation(lastOperatorInStack)
-                        && hasHigherOrEqualPriority(lastOperatorInStack, currentSymbol)) {
-                    expressionToCalculate.push(mathOperationsStack.pop());
-                }
-                mathOperationsStack.push(currentSymbol);
-            }
+            putOpenBracketInStackIfPresent(currentSymbol);
+            manipulateWithExpressionInBrackets(currentSymbol);
+            manipulateWithMathOperation(currentSymbol);
         }
 
+        finishExpressionParsing();
+
+        return expressionToCalculate;
+    }
+
+    /**
+     * Clears  {@code expressionToCalculate} and {@code mathOperationsStack}
+     * before each parsing
+     */
+    public void clearStacks() {
+        expressionToCalculate.clear();
+        mathOperationsStack.clear();
+    }
+
+    /**
+     * Searches digits or unary minus operator and puts them into {@code expressionToCalculate}
+     *
+     * @param inputString - String to be parsed
+     * @return {@code true} if digit or unary minus operator found, false if not
+     */
+    public boolean isDigitOrUnaryMinusFound(StringBuilder inputString) {
+        Matcher matcher = DIGIT_PATTERN.getPattern().matcher(inputString);
+        boolean isFound;
+        if (isFound = matcher.find()) {
+            expressionToCalculate.push(matcher.group());
+            inputString.delete(0, matcher.end());
+        }
+
+        if (matcher.usePattern(UNARY_MINUS_PATTERN.getPattern()).find()) {
+            inputString.delete(0, matcher.end());
+            mathOperationsStack.push(OPEN_BRACKET);
+            expressionToCalculate.push(String.valueOf(0));
+            mathOperationsStack.push(MINUS.getOperator());
+            isFound = true;
+        }
+
+        return isFound;
+    }
+
+    /**
+     * Compares {@code currentSymbol} with {@code OPEN_BRACKET} and puts it into {@code mathOperationsStack}
+     *
+     * @param currentSymbol - symbol to be compared to {@code OPEN_BRACKET}
+     */
+    public void putOpenBracketInStackIfPresent(String currentSymbol) {
+        if (currentSymbol.equals(OPEN_BRACKET)) {
+            mathOperationsStack.push(currentSymbol);
+        }
+    }
+
+    /**
+     * Compares {@code currentSymbol} with {@code CLOSED_BRACKET} and puts all math operators from
+     * {@code mathOperationsStack} into {@code expressionToCalculate} until it meets {@code OPEN_BRACKET}
+     *
+     * @param currentSymbol - symbol to be compared to {@code CLOSED_BRACKET}
+     * @throws IncorrectBracketsException if {@code OPEN_BRACKET} is not found
+     */
+    public void manipulateWithExpressionInBrackets(String currentSymbol) throws IncorrectBracketsException {
+        if (currentSymbol.equals(CLOSED_BRACKET)) {
+            String lastOperatorInStack;
+            try {
+                while (!(lastOperatorInStack = mathOperationsStack.pop()).equals(OPEN_BRACKET)) {
+                    expressionToCalculate.push(lastOperatorInStack);
+                }
+            } catch (NoSuchElementException e) {
+                throw new IncorrectBracketsException(INCORRECT_BRACKETS.toString());
+            }
+        }
+    }
+
+    /**
+     * If {@code currentSymbol} is math operator, then method pushes all math operators to {@code expressionToCalculate}
+     * from {@code mathOperationsStack}, whose priority equals or higher than {@code currentSymbol}
+     *
+     * @param currentSymbol - symbol to be compared to {@code MathOperator}
+     */
+    public void manipulateWithMathOperation(String currentSymbol) {
+        if (isMathOperation(currentSymbol)) {
+            String lastOperatorInStack;
+            while ((lastOperatorInStack = mathOperationsStack.peek()) != null
+                    && isMathOperation(lastOperatorInStack)
+                    && hasHigherOrEqualPriority(lastOperatorInStack, currentSymbol)) {
+                expressionToCalculate.push(mathOperationsStack.pop());
+            }
+            mathOperationsStack.push(currentSymbol);
+        }
+    }
+
+    /**
+     * Takes all elements from {@code mathOperationsStack} and pushes them into
+     * {@code expressionToCalculate}
+     *
+     * @throws IncorrectBracketsException thrown if {@code mathOperationsStack} contains {@code OPEN_BRACKET}
+     */
+    public void finishExpressionParsing() throws IncorrectBracketsException {
         if (mathOperationsStack.contains(OPEN_BRACKET)) {
             throw new IncorrectBracketsException(INCORRECT_BRACKETS.toString());
         } else {
             mathOperationsStack.forEach(expressionToCalculate::push);
-            return expressionToCalculate;
         }
     }
 
@@ -84,6 +151,7 @@ public class StringParser implements IStringParser {
      * @param textToCheck the text which has to be checked whether it is math operator or not
      * @return {@code true} String is arithmetical operation
      */
+    @Override
     public boolean isMathOperation(String textToCheck) {
         return contains(textToCheck);
     }
@@ -93,6 +161,7 @@ public class StringParser implements IStringParser {
      * @param secondOperation arithmetical operation represented by String
      * @return {@code true} if first argument has higher priority
      */
+    @Override
     public boolean hasHigherOrEqualPriority(String firstOperation, String secondOperation) {
         return Objects.requireNonNull(findByOperator(firstOperation)).getPriority()
                 .compareTo(Objects.requireNonNull(findByOperator(secondOperation)).getPriority()) >= 0;
